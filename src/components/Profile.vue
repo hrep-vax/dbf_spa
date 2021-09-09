ca5c437401d973ff217105732a7b4837a018a0ae
hrep-icts <regenciafelixaram@gmail.com>
2021-09-09 18:55:35 +0800

    Added spinners and toast to profile and login

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -28,8 +28,12 @@
                         <img v-else :src="profilePictureSrc" class="w-full h-full object-cover">
                     </span>
                     <input type="file" id="upload_file" name="upload_file" ref="file" v-on:change="changeProfilePic()" hidden>
-                    <label for="upload_file" refs="upload_file" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                        Change
+                    <label for="upload_file" refs="upload_file" class="flex justify-center ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                        <svg v-if="isProfilePictureLoading" class="animate-spin h-5 w-5 mx-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        <p v-else>Change</p>
                     </label>
                 </div>
             </div>
@@ -221,8 +225,8 @@
                         <button type="submit" :disabled="invalid" class="disabled:opacity-50 disabled:cursor-not-allowed group relative flex py-2 px-5 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                             <svg v-if="isLoading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
+                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
                             <p v-else>Save</p>
                         </button>
                     </div>
@@ -370,7 +374,6 @@
             async getUserProfilePicture() {
                 
                 try {
-                    
                     this.isProfilePictureLoading = true
                     let imgURL = await this.handleGetUserProfilePic()
                     this.profilePictureSrc = imgURL;
@@ -410,8 +413,12 @@
                     let user = await this.handleUploadUserProfilePic(formData);
                     console.log(user)
 
-                    //profile pic changed successfully 
                     this.profilePictureSrc = window.URL.createObjectURL(this.profilePictureToUpload)
+                    
+                    Vue.$toast.open({
+                        message: "Profile picture updated successfully!",
+                        type: "success"
+                    });
                 }
                 catch (error) {
                     console.log(error.response)
