0bbec9296f7fe84f44530125d37bb9320fb33093
hrep-icts <regenciafelixaram@gmail.com>
2021-09-10 21:30:51 +0800

    Changed Landing Login to Sign In

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -12,20 +12,71 @@
       </p>
       <div class="mt-5 sm:mt-8 flex justify-center">
         <div v-if="!getUserIsAuthenticated" class="rounded-md shadow">
-          <a href="/login"
-             class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
-            Login
+          <a
+            href="/login"
+            class="
+              w-full
+              flex
+              items-center
+              justify-center
+              px-8
+              py-3
+              border border-transparent
+              text-base
+              font-medium
+              rounded-md
+              text-white
+              bg-blue-600
+              hover:bg-blue-700
+              md:py-4 md:text-lg md:px-10
+            "
+          >
+            Sign In
           </a>
         </div>
         <div v-if="!getUserIsAuthenticated" class="mt-3 sm:mt-0 sm:ml-3">
-          <a href="/register"
-             class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-            Sign-up
+          <a
+            href="/register"
+            class="
+              w-full
+              flex
+              items-center
+              justify-center
+              px-8
+              py-3
+              border border-transparent
+              text-base
+              font-medium
+              rounded-md
+              text-blue-600
+              bg-blue-100
+              hover:bg-blue-200
+              md:py-4 md:text-lg md:px-10
+            "
+          >
+            Sign Up
           </a>
         </div>
         <div v-if="getUserIsAuthenticated" class="mt-3 sm:mt-0 sm:ml-3">
-          <a href="/dashboard/home"
-             class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+          <a
+            href="/dashboard/home"
+            class="
+              w-full
+              flex
+              items-center
+              justify-center
+              px-8
+              py-3
+              border border-transparent
+              text-base
+              font-medium
+              rounded-md
+              text-blue-600
+              bg-blue-100
+              hover:bg-blue-200
+              md:py-4 md:text-lg md:px-10
+            "
+          >
             Go to Home Page
           </a>
         </div>
@@ -35,31 +86,31 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex';
 const navigation = [
-  {name: 'Product', href: '#'},
-  {name: 'Features', href: '#'},
-  {name: 'Marketplace', href: '#'},
-  {name: 'Company', href: '#'},
-]
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+];
 
 export default {
   components: {},
   setup() {
     return {
       navigation,
-    }
+    };
   },
   computed: {
-    ...mapGetters(['getUserIsAuthenticated', 'getAuthUser'])
+    ...mapGetters(['getUserIsAuthenticated', 'getAuthUser']),
   },
   methods: {
     gotoRegister() {
-      this.$router.push({path: "/register"})
+      this.$router.push({ path: '/register' });
     },
     gotoLogin() {
-      this.$router.push({path: "/login"})
-    }
+      this.$router.push({ path: '/login' });
+    },
   },
-}
+};
 </script>
