58537f2bb875fcb8ab7fdd02db1c34c5af71b5fe
hrep-icts <regenciafelixaram@gmail.com>
2021-09-14 14:18:50 +0800

    Added Inter as the default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: {content: ["./public/**/*.html", "./src/**/*.vue"]},
   darkMode: false, // or 'media' or 'class'
